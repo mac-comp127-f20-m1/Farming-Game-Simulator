a2c976f5f5eaba38619155ac1ec4a0b330cd6b7a
Russell Ni <70660479+cniruss0727@users.noreply.github.com>
2020-10-14 16:10:52 -0500

    Update 10.14

--- src/Character.java
+++ src/Character.java
@@ -1,3 +1,4 @@
+import edu.macalester.graphics.CanvasWindow;
 import edu.macalester.graphics.Ellipse;
 import edu.macalester.graphics.Image;
 import edu.macalester.graphics.GraphicsGroup;
@@ -8,13 +9,53 @@ public class Character {
     // private double speed;
     GraphicsGroup graphics = new GraphicsGroup(0, 0);
     // buildGraphics();
+    private Image pig;
     
+    
+   
+        
     
     public Character(){
-        Image pig = new Image(0, 0, "pig.png");
-        pig.setMaxHeight(50);
-        pig.setMaxWidth(50);
+        pig = new Image(0, 0, "pig.png");
+        pig.setMaxHeight(100);
+        pig.setMaxWidth(80);
         graphics.add(pig);
+        
+    }
+
+    // public void movePaddle(Point point) {
+    //     this.setCenter(point.withY(600));
+    //     double newx = getX();
+    //     if (newx > 0 && newx + this.paddleWidth < maxX) {
+    //         this.setPosition(newx, getY());
+    //     }
+    //     if (newx <= 0) {
+    //         this.setPosition(0, getY());
+    //     }
+    //     if (newx + this.paddleWidth >= maxX) {
+    //         this.setPosition(maxX - this.paddleWidth, getY());
+    //     }
+    // }
+
+    public void addToCanvas(CanvasWindow canvas){
+        canvas.add(this.pig);
+    }
+
+    public void moveX(double dx){
+        pig.setX(pig.getX()+dx);
+    }
+
+    public void moveY(double dy){
+        pig.setY(pig.getY()+dy);
+    }
+
+
+    public double getX(){
+        return pig.getX();
+    }
+
+    public double getY(){
+        return pig.getY();
     }
     
 
--- src/Environment.java
+++ src/Environment.java
@@ -1,6 +1,64 @@
-import macalester.edu.Graphics
+
+import edu.macalester.graphics.CanvasWindow;
+import edu.macalester.graphics.GraphicsObject;
+import edu.macalester.graphics.Rectangle;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.awt.Color;
 
 
 public class Environment {
+    private double landPlot=250;
+    private int Row = 3, brickColumn = 3;
+    private List<GraphicsObject> lands = new ArrayList<GraphicsObject>();
+
+    public Environment() {
+        for (int j = 0; j < Row; j++) {
+
+            for (int i = 0; i < Row; i++) {
+               Rectangle rec = new Rectangle(i,j, landPlot,
+                landPlot);
+                rec.setPosition(i*landPlot+70*i,j*landPlot+70*j);
+                rec.setFillColor(new Color(139,69,19));
+                lands.add(rec);
+              
+             
+             
+            }
+        }
+    }
+
+    // // public void createBrickWall(){
+
+    //     for (int y=100;y<=350;y+=25){
+    //         double x=5;
+    //         for (int j=0;j<10;j++){
+    //             Brick brick= new Brick(x, y, 50, 20);
+    //             brick.addToCanvas(window);
+                
+    //             // bricks.add(brick);
+    //             x+=60;
+    //         }
+    //     } 
+
+   
+
+    public List<GraphicsObject> getLands() {
+        return lands;
+    }
+
+    
+    public void addToCanvas(CanvasWindow canvas) {
+        for (GraphicsObject b : lands) {
+            canvas.add(b);
+        }
+    }
+
+    public void setColor(Rectangle rec){
+
+    }
+
     
 }
+
