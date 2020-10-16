33a9c56f3f6adae15f49fe0e524322b94af39f61
Jaylynn Rodriguez <70664722+jrworm@users.noreply.github.com>
2020-10-16 16:14:27 -0400

    made Plant interface & Apple class

---
+++ src/Apple.java
@@ -0,0 +1,38 @@
+import edu.macalester.graphics.GraphicsGroup;
+import edu.macalester.graphics.Image;
+import edu.macalester.graphics.CanvasWindow;
+
+public class Apple implements Plant {
+
+    GraphicsGroup graphics = new GraphicsGroup(0, 0);
+    private int price;
+    private Image apple;
+
+    public Apple() {
+        apple = new Image(0, 0, "apple.png");
+        apple.setMaxHeight(100);
+        apple.setMaxWidth(80);
+        graphics.add(apple);
+    }
+
+    public void growLarger() {
+        // makes the plant grow larger
+    }
+
+    public boolean maxSize() {
+        // check if plant is at maximum size
+        return true;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void addToCanvas(CanvasWindow canvas) {
+        canvas.add(apple);
+    }
+
+    public void removeFromCanavas(CanvasWindow canvas) {
+        canvas.remove(apple);
+    }
+}
